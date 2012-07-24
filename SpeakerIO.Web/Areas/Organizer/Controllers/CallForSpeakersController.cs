26a57f36060499ac19956f40e760a2bd86c43b0e
Matt Hinze <mhinze@gmail.com>
2012-07-24 17:14:33 -0500

    correct spelling 'call for speaker' => 'call for speakers'

---
+++ SpeakerIO.Web/Areas/Organizer/Controllers/CallForSpeakersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using SpeakerIO.Web.Areas.Organizer.Models;
+
+namespace SpeakerIO.Web.Areas.Organizer.Controllers
+{
+    public class CallForSpeakersController : Controller
+    {
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View(new CallForSpeakersInput());
+        }
+
+        [HttpPost, ActionName("Create")]
+        public ActionResult PostCreation(CallForSpeakersInput input)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
