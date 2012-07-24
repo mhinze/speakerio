ffe3850bae51eefdcf3775ff4ee120a60b35d2bf
Matt Hinze <mhinze@gmail.com>
2012-07-24 10:27:51 -0500

    create a call for speaker initial stuff

--- SpeakerIO.Web/Areas/Organizer/Controllers/CallForSpeakerController.cs
+++ SpeakerIO.Web/Areas/Organizer/Controllers/CallForSpeakerController.cs
@@ -5,9 +5,20 @@ namespace SpeakerIO.Web.Areas.Organizer.Controllers
 {
     public class CallForSpeakerController : Controller
     {
+        [HttpGet]
         public ViewResult Create()
         {
             return View(new CallForSpeakerViewModel());
+        }
+
+        [HttpPost, ActionName("Create")]
+        public ActionResult PostCreation(CallForSpeakerViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
         }
     }
 }
--- SpeakerIO.Web/Areas/Organizer/Models/CallForSpeakerViewModel.cs
+++ SpeakerIO.Web/Areas/Organizer/Models/CallForSpeakerViewModel.cs
@@ -1,7 +1,28 @@
-﻿namespace SpeakerIO.Web.Areas.Organizer.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpeakerIO.Web.Areas.Organizer.Models
 {
     public class CallForSpeakerViewModel
     {
-         
+        [Required]
+        [DisplayName("Event Name")]
+        public string EventName { get; set; }
+        
+        [Required]
+        [DisplayName("Event Description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public IEnumerable<DateTime> DaysOfEvent { get; set; }
+
+        [DisplayName("Last Day To Submit")]
+        public DateTime? LastDayToSubmit { get; set; }
+        
+        [DisplayName("Event Logo URL")]
+        [DataType(DataType.Url)]
+        public string LogoUrl { get; set; }
     }
 }
