d48e01fc656002f103d5366acc7ada642d1c589f
Matt Hinze <mhinze@gmail.com>
2012-07-24 10:03:16 -0500

    organizer area

--- SpeakerIO.Web/App_Start/RouteConfig.cs
+++ SpeakerIO.Web/App_Start/RouteConfig.cs
@@ -22,8 +22,8 @@ namespace SpeakerIO.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Account", action = "Login" }
             );
         }
     }
