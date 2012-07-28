17c7db12701953db8303c03907fa1a7ebcd27939
Matt Hinze <mhinze@gmail.com>
2012-07-28 11:43:44 -0500

    can edit a call for speaker

--- SpeakerIO.Web/Areas/Organizer/Controllers/CallForSpeakersController.cs
+++ SpeakerIO.Web/Areas/Organizer/Controllers/CallForSpeakersController.cs
@@ -1,5 +1,5 @@
-﻿using System.Data;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using RestSharp.Contrib;
 using SpeakerIO.Web.Areas.Organizer.Models;
 using SpeakerIO.Web.Data;
 using SpeakerIO.Web.Data.Model;
@@ -12,7 +12,53 @@ namespace SpeakerIO.Web.Areas.Organizer.Controllers
         [HttpGet]
         public ViewResult Create()
         {
-            return View(new CallForSpeakersInput());
+            return View("Create", new CallForSpeakersInput());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(User user, long id)
+        {
+            using (var db = new DataContext(user))
+            {
+                var found = db.CallsForSpeakers.Find(id);
+                if (found == null || found.User.Id != user.Id)
+                {
+                    return InvalidEdit();
+                }
+                return View("Create", new CallForSpeakersInput(found));
+            }
+        }
+
+        [HttpPost, ActionName("Edit")]
+        public ActionResult ProcessEdit(CallForSpeakersInput input, User user)
+        {
+            if (input.Id == null)
+            {
+                return InvalidEdit();
+            }
+            if (ModelState.IsValid)
+            {
+                using (var db = new DataContext(user))
+                {
+                    var found = db.CallsForSpeakers.Find(input.Id);
+                    if (found.User.Id != user.Id)
+                    {
+                        return InvalidEdit();
+                    }
+                    found.UpdateFrom(input);
+                    db.SaveChanges();
+
+                    TempData["success"] = "You edited call for speaker for " + found.EventName; // encoded in view
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View("Create");
+        }
+
+        ActionResult InvalidEdit()
+        {
+            TempData["error"] = "Invalid edit";
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost, ActionName("Create")]
