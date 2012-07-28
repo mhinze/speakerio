55c4eb9aa539b6bc2889168213663a7c4f22c3b0
Matt Hinze <mhinze@gmail.com>
2012-07-28 10:35:39 -0500

    call for speaker list on organizer home page

---
+++ SpeakerIO.Web/Application/Extensions/DateFormatExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpeakerIO.Web.Application.Extensions
+{
+    public static class DateFormatExtensions
+    {
+         public static string ToDate(this DateTime? date)
+         {
+             if (date == null)
+                 return string.Empty;
+             return date.Value.ToShortDateString();
+         }
+    }
+}
--- SpeakerIO.Web/Areas/Organizer/Models/HomeViewModel.cs
+++ SpeakerIO.Web/Areas/Organizer/Models/HomeViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace SpeakerIO.Web.Areas.Organizer.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+using SpeakerIO.Web.Data.Model;
+
+namespace SpeakerIO.Web.Areas.Organizer.Models
 {
     public class HomeViewModel
     {
-        public int CallForSpeakersCount { get; set; }
+        readonly CallForSpeakers[] _calls;
+
+        public HomeViewModel(IEnumerable<CallForSpeakers> calls)
+        {
+            _calls = (calls ?? Enumerable.Empty<CallForSpeakers>()).ToArray();
+        }
+
+        public CallForSpeakers[] CallsForSpeakers
+        {
+            get { return _calls.ToArray(); }
+        }
+
+        public int CallForSpeakersCount
+        {
+            get { return _calls.Length; }
+        }
     }
 }
