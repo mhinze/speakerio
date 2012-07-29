105b93b15cc2049a351261dc081ba89dbf919cf4
Eric Hexter <eric.hexter@gmail.com>
2012-07-29 12:03:06 -0500

    enables auto migrations for app harbor

--- SpeakerIO.Web/Migrations/Configuration.cs
+++ SpeakerIO.Web/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SpeakerIO.Web.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(SpeakerIO.Web.Data.DataContext context)
