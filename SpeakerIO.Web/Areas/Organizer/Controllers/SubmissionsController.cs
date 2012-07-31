f3507accbd0d56fdfaa524fb56f5019194ca368d
Matt Hinze <mhinze@gmail.com>
2012-07-30 21:39:23 -0500

    submissions page

---
+++ SpeakerIO.Web/Areas/Organizer/Controllers/SubmissionsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SpeakerIO.Web.Controllers;
+using SpeakerIO.Web.Data;
+using SpeakerIO.Web.Data.Model;
+
+namespace SpeakerIO.Web.Areas.Organizer.Controllers
+{
+    [Authorize]
+    public class SubmissionsController : BaseController
+    {
+        public ActionResult Review(User user, long id)
+        {
+            using (var db = new DataContext(user))
+            {
+                var call = db.CallsForSpeakers.SingleOrDefault(c => c.Id == id && c.User.Id == user.Id);
+                {
+                    if (call == null)
+                    {
+                        Error("Invalid call for speakers");
+                        return RedirectToAction("Index", "Home");
+                    }
+                    var submissions = db.Submissions.Where(x => x.CallForSpeakers.Id == id);
+                    return View(submissions.ToArray());
+                }
+            }
+        }
+    }
+}
