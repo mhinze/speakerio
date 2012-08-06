b7e64bf5d4609c180b031ea9bda681dbd9367911
Matt Hinze <mhinze@gmail.com>
2012-08-05 23:34:33 -0500

    my submissions - details

--- SpeakerIO.Web/Areas/Speaker/Controllers/MySubmissionsController.cs
+++ SpeakerIO.Web/Areas/Speaker/Controllers/MySubmissionsController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Web.Mvc;
+using SpeakerIO.Web.Controllers;
 using SpeakerIO.Web.Data;
 using SpeakerIO.Web.Data.Model;
 using System.Linq;
 
 namespace SpeakerIO.Web.Areas.Speaker.Controllers
 {
-    public class MySubmissionsController : Controller
+    public class MySubmissionsController : BaseController
     {
         public ViewResult Index(User speaker)
         {
@@ -20,7 +21,21 @@ namespace SpeakerIO.Web.Areas.Speaker.Controllers
                     .ThenBy(x => x.Title)
                     .ToArray();
 
-                return View(all);
+                return View("Index", all);
+            }
+        }
+
+        public ActionResult Submission(User speaker, long id)
+        {
+            using (var db = new DataContext(speaker))
+            {
+                var sub = db.Submissions.Include("CallForSpeakers").SingleOrDefault(x => x.Id == id && x.Speaker.Id == speaker.Id);
+                if (sub == null)
+                {
+                    Error("Invalid submission");
+                    return RedirectToAction("Index", "MySubmissions");
+                }
+                return View(sub);
             }
         }
     }
